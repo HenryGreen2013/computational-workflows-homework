6bb677726d3cf93e371c570425b040a3b2bc56a0
Henry <jinyuan.wang@uni.lu>
2019-12-05 15:14:42 +0100

    Henry

---
+++ Command.sh
@@ -0,0 +1,26 @@
+sudo docker build .
+# Successfully built 7897da544514
+
+sudo docker tag 78 henrygreen/pytest
+sudo docker login
+sudo docker push henrygreen/pytest
+# latest: digest: sha256:c8f7ff35288a1890ac7c54bdb45987f508fa5dbc3a28caa8154ae7dcfbba16ed size: 1364
+
+cd computational-workflows-homework
+git init
+git add .
+git commit -m "version01" # not sure what it means
+git remote add origin https://github.com/HenryGreen2013/computational-workflows-homework
+git pull origin master
+git push origin master
+git add .
+# copy the Dockerfile into the current folder
+git add .
+git commit -m "version01"
+git push origin master
+
+# copy wallet.py and test_wallet.py
+git add .
+git commit -m "version01"
+git push origin master
+
